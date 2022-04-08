0f1eee248dc9462fa3868bdd37417a8a2b0f4956
rPbl3yH <tnv2205@gmail.com>
2022-04-08 11:24:49 +0500

    Add draw line to circle

--- Assets/Scripts/Circle.cs
+++ Assets/Scripts/Circle.cs
@@ -9,8 +9,10 @@ public class Circle : MonoBehaviour
     [SerializeField] Vector2 _currentWayPoint;
     public List<Vector2> _wayPoints = new List<Vector2>();
     [SerializeField] Slider _slider;
+    [SerializeField] bool _canDraw;
 
     int _curIdWayPoint = 0;
+    //int _curIdWayPointForLoop = 0;
 
     LineRenderer _line;
 
@@ -21,9 +23,8 @@ public class Circle : MonoBehaviour
      
         _slider = GameObject.FindGameObjectWithTag("Slider").GetComponent<Slider>();
         _line = GetComponent<LineRenderer>();
-        _line.positionCount = 0;
-
-
+        
+        
         
     }
 
@@ -32,27 +33,40 @@ public class Circle : MonoBehaviour
     {
         _speed = _slider.value;
         
-        if(_curIdWayPoint >= 1)
+        if(_wayPoints.Count >= 2)
         {
             
             if (Vector3.Distance(transform.position, _currentWayPoint) <= 0.1)
             {
-
+                _canDraw = true;
                 _currentWayPoint = _wayPoints[_curIdWayPoint];
-                if (_curIdWayPoint < _wayPoints.Count)
+                if (_curIdWayPoint < _wayPoints.Count - 1)//Чтобы индекс не стал длинее массива
                     _curIdWayPoint++;
+            }
+            else
+            {
+                _canDraw = false;
             }
 
             transform.position = Vector3.MoveTowards(
             transform.position,
             _currentWayPoint,
             _speed * Time.deltaTime);
+
+            _line.positionCount = _curIdWayPoint;
+
+            for (int index = 0; index < _curIdWayPoint; index++)
+            {
+
+                Vector3 posPoint = _wayPoints[index];
+                posPoint.z = -1;
+
+                _line.SetPosition(index, posPoint);//Т.к. вначале добавил один
+
+            }
             
         }
-        else
-        {
-            _curIdWayPoint = _wayPoints.Count - 1;
-        }
+        
                 
     }
 
